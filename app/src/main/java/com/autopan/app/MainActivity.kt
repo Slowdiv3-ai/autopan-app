2f25c9e220ee9b1608a8736fcd40bf98aa9a30c9
Slowdiv3-ai <redmihyperos10c@gmail.com>
2026-08-18 19:08:22 +0300

    Add Intent import to MainActivity.kt

--- app/src/main/java/com/autopan/app/MainActivity.kt
+++ app/src/main/java/com/autopan/app/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.bluetooth.BluetoothDevice
 import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
 import android.content.IntentFilter
 import android.graphics.Color
 import android.os.Bundle
